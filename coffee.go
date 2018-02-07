cd062799ee427727e29fe7d6fa9fd80e67793d2b
Makpoc <makpoc@gmail.com>
2018-02-07 11:47:54 +0200

    Fix message format

--- coffee.go
+++ coffee.go
@@ -8,15 +8,16 @@ import (
 
 // coffeeHandler responds to coffee command with some recepies
 func coffeeHandler(s *discordgo.Session, m *discordgo.MessageCreate, command commandWithArgs) {
-	_, err := s.ChannelMessageSend(m.ChannelID, `*1.* Brew a large espresso :coffee:.
+	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf(`Here's the recipe you asked for %s
+		*1.* Brew a large espresso :coffee:.
     *2.* Fill a cocktail shaker half full with ice cubes :cocktail:.
     *3.* Add to the ice the brewed espresso.
     *4.* 3 tablespoons vodka.
     *5.* 3 tablespoons Kahlúa (coffee liqueur)
     *6.* 1/4 teaspoon sugar.
-		*7.* Shake until foamy, about 30 seconds; strain into a martini glass.
-		*8.* Give to someone else and get a :beer: or 3
-		*9.* Enjoy :beers`)
+    *7.* Shake until foamy, about 30 seconds; strain into a martini glass.
+    *8.* Give to someone else and get a :beer: or 3
+    *9.* Enjoy :beers:`, m.Author.Mention)
 	if err != nil {
 		fmt.Println(err)
 	}
